bbed7f8896608b5cdf5e73e3c5dd54a0d056e0b0
ViniNeko <viniciusdecruz1995@gmail.com>
2025-06-02 02:14:53 +0000

    Trabalho das CartasSuperTrunfo (parte1)

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,17 +6,49 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
-int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+int main(){
+    int Pop1, Tur1, Pop2, Tur2;
+    float Area1, PIB1, Area2, PIB2;
+        
+printf("Cadastrar duas cartas para o jogo do Super Trunfo. Estas cartas terão temas de cidade. Vamos começar? \n");
 
-    return 0;
+printf("\nPrimeiramente, a carta número 1. \n");
+printf("\nDigite a população da cidade: \n");
+scanf("%d", &Pop1);
+
+printf("Digite a área da cidade: \n");
+scanf("%f", &Area1);
+
+printf("Digite o PIB da cidade: \n");
+scanf("%f", &PIB1);
+
+printf("Digite o número de pontos turísticos que a cidade possui: \n");
+scanf("%d", &Tur1);
+
+printf("\nAgora, vamos cadastrar a carta número 2. Tente digitar dados diferentes desta vez. \n");
+printf("\nDigite a população da cidade: \n");
+scanf("%d", &Pop2);
+
+printf("Digite a área da cidade: \n");
+scanf("%f", &Area2);
+
+printf("Digite o PIB da cidade: \n");
+scanf("%f", &PIB2);
+
+printf("Digite o número de pontos turísticos que a cidade possui: \n");
+scanf("%d", &Tur2);
+
+printf("\nParabéns! Você cadastrou com sucesso duas cartas para o Super Trunfo. Vamos ver as informações das cartas? \n");
+
+printf("\n Carta número 1 \n");
+printf("População: %d \nÁrea: %f\n", Pop1, Area1);
+printf("PIB: %f \nNúmero de pontos turísticos: %d \n", PIB1, Tur1);
+
+printf("\n Carta número 2 \n");
+printf("População: %d \nÁrea: %f\n", Pop2, Area2);
+printf("PIB: %f \nNúmero de pontos turísticos: %d \n", PIB2, Tur2);
+
+
+return 0;
+
 }
